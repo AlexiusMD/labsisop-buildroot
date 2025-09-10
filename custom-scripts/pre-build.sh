b8fe609a35520899812b7d55d58c40712a20a150
Alexius Maliuk Dias <132586837+AlexiusMD@users.noreply.github.com>
2025-09-10 21:15:51 +0000

    Add syscall and all of linux kernel

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
-chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+if [-d "$BASE_DIR" ] then
+    cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
+    chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+fi
 
-cp $BASE_DIR/../custom-scripts/systeminfo.py $BASE_DIR/target/etc/init.d
-chmod +x $BASE_DIR/target/etc/init.d/systeminfo.py
 
-cp $BASE_DIR/../custom-scripts/S50systeminfo $BASE_DIR/target/etc/init.d
-chmod +x $BASE_DIR/target/etc/init.d/S50systeminfo
+# cp $BASE_DIR/../custom-scripts/systeminfo.py $BASE_DIR/target/etc/init.d
+# chmod +x $BASE_DIR/target/etc/init.d/systeminfo.py
+
+# cp $BASE_DIR/../custom-scripts/S50systeminfo $BASE_DIR/target/etc/init.d
+# chmod +x $BASE_DIR/target/etc/init.d/S50systeminfo
